4f7c1b8d0b7501618e9017f72a00efdfcdf5ccd7
Arthur Lange <162444011+Arthur-lange@users.noreply.github.com>
2024-09-11 09:12:35 -0300

    Add files via upload

---
+++ calculadora 04-09/scripts.js/script.js
@@ -0,0 +1,60 @@
+var numero = ''
+var operadorAtual = ''
+var numeroAnterior = ''
+
+function addNumero(caracter){
+    numero += caracter
+    document.getElementById('display').value = numero
+}
+
+function limparDisplay(){
+    numero = ''
+    operadorAtual = ''
+    numeroAnterior = ''
+    document.getElementById('display').value = ''
+}
+
+function apagar(){
+    let display = document.getElementById('display').value
+    document.getElementById('display').value = display.slice(0,-1)
+}
+
+function operador(op){
+    if(numero != ''){
+        operadorAtual = op
+        numeroAnterior = numero
+        numero = ''
+        document.getElementById('display').value = ''
+    }
+}
+
+
+function calcular(){
+    
+    let resultado = 0
+    let atual = parseFloat(numero)
+    let anterior = parseFloat(numeroAnterior)
+    
+
+
+    if (operadorAtual === '+'){
+        resultado = anterior + atual
+    }else if(operadorAtual === '-'){
+        resultado = anterior - atual
+    }else if(operadorAtual === '*'){
+        resultado = anterior * atual
+    }else if(operadorAtual === '/'){
+        if(atual === 0){
+            document.getElementsById('display').value = 'Erro: divisão por zero'
+            return
+        }
+        resultado = anterior / atual
+    }
+    
+
+    document.getElementById('display').value = resultado
+    numero = resultado
+    operadorAtual = ''
+    numeroAnterior = ''
+    
+}
